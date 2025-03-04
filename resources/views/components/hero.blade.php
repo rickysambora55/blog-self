a10946a19c2843a3d4671a0b768ccd6d3759b2e5
Ricky <rickysambora55@gmail.com>
2025-03-04 17:52:02 +0700

    hero padding

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,11 +1,11 @@
-<section class="w-full h-184 px-8 bg-gray-100" id="home">
+<section class="w-full h-184 px-16 bg-gray-100" id="home">
     <div class="container h-full py-24 flex flex-col md:flex-row gap-8 mx-auto items-center">
         <div class="flex flex-col w-full justify-center text-center md:text-left">
             <span class="text-2xl">Hi, I'm</span>
             <span class="text-6xl font-bold">Uni</span>
             <span class="text-xl">I'am a Fullstack Developer</span>
         </div>
-        <div class="flex w-full h-2/3 md:h-full items-center justify-center">
+        <div class="flex w-full h-2/3 md:h-full items-center justify-end">
             <img src="{{url('/img/Person_Hero.webp')}}" class="h-auto max-h-full aspect-auto object-top"
                 alt="User Picture" />
         </div>
