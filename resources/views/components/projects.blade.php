dfadbaecf34168190d50dfcf99fc9f7940b040e0
Ricky <rickysambora55@gmail.com>
2025-03-10 16:25:44 +0700

    project page

--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -9,7 +9,8 @@
 
         <div class="w-full 2xl:px-2 flex flex-wrap justify-evenly gap-8 mt-6">
             @foreach ($projects as $project)
-            <x-project-item :images="$project['images']" :technologies="$project['technologies']">
+            <x-project-item href="project/{{$project['slug']}}" :images="$project['images']"
+                :technologies="$project['technologies']">
                 <x-slot:title>{{$project['title']}}</x-slot:title>
                 <x-slot:type>{{$project['type']}}</x-slot:type>
                 <x-slot:description>{{$project['description']}}</x-slot:description>
