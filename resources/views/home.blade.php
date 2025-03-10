65ac25efbc6324dc31003bd889346d04582fd581
Ricky <rickysambora55@gmail.com>
2025-03-10 10:24:01 +0700

    remove vercel deployment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Name</title>
 
-        @vite('resources/css/app.css')
-        @vite('resources/js/app.js')
+        @vite(['resources/css/app.css','resources/js/app.js'])
 
         <style>
             [x-cloak] {
